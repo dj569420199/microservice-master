422e226582bfc1d98b2187233a558cdcb64659a0
Deakin <dj569420199@126.com>
2020-08-02 23:16:54 +0800

    comfig-client

--- ms-config-client/src/main/java/com/micro/demo/MsConfigClientApplication.java
+++ ms-config-client/src/main/java/com/micro/demo/MsConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.micro.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsConfigClientApplication.class, args);
