0a46e18dbec68347cf91cf5fd77033a725225b6e
flyingfish <dj569420199@126.com>
2020-08-11 21:08:15 +0800

    ms-admin-server

---
+++ ms-admin-server/src/main/java/com/micro/demo/MsAdminServerApplication.java
@@ -0,0 +1,14 @@
+package com.micro.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MsAdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsAdminServerApplication.class, args);
+    }
+}
